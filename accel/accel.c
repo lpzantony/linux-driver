71d66e2cf157c17519cfc21e633aeb526e747acc
Antony <antony.lopez@telecom-paristech.fr>
2016-11-15 17:02:19 +0100

    compilable acce.c subscribed to misc framework. Empty implementation of ioctl and open

--- accel/accel.c
+++ accel/accel.c
@@ -3,8 +3,10 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/i2c.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
 
-/* I2C specific code */
+// I2C Registers
 #define DEVID           0x00
 #define BW_RATE         0x2C //0x0A for 100Hz
 #define INT_ENABLE      0x2E //0x00 for no interrupt
@@ -12,14 +14,11 @@
 #define FIFO_CTL        0x38
 #define POWER_CTL       0x2D
 
+// Values
 #define RATE_100_HZ     0x0A
 #define RESET_VAL       0x00
 #define MEASURE_MODE    0x08
 #define STANDBY_MODE    0x00
-
-static char *testmsg = "Hello i2c!";
-module_param(testmsg, charp, S_IRUGO);
-MODULE_PARM_DESC(testmsg, "a debug message");
 
 void init_accel(struct i2c_client *client)
 {
@@ -49,11 +48,59 @@ void init_accel(struct i2c_client *client)
         sendBuf[1] = MEASURE_MODE;
         retval = i2c_master_send(client,sendBuf, 2);
 }
+
+static int accel_read(struct file *fd, char __user *usr, size_t size, loff_t * offset)
+{
+        return 0;
+}
+
+static int accel_ioctl(struct file *fd, unsigned int cmd, unsigned long arg)
+{
+        return 0;
+}
+
+struct accel_device {
+    /* Données propres à un périphérique (exemple) */
+    int used_channel;
+    /* Le périphérique misc correspondant */
+    struct miscdevice miscdev;
+};
+
 static int accel_probe(struct i2c_client *client,
                      const struct i2c_device_id *id)
 {
-        init_accel(client);
+        // structure de callback
+        struct file_operations accel_fops = {
+                .read  = accel_read ,
+                .unlocked_ioctl = accel_ioctl
+        };
 
+        // MISC Framework
+        struct accel_device *acceldev;
+        int ret;
+
+        /* Alloue la mémoire pour une nouvelle structure foo_device */
+        acceldev = devm_kzalloc(&client->dev, sizeof(struct accel_device), GFP_KERNEL);
+        if (!acceldev) return -ENOMEM;
+
+        /* Initialise la structure foo_device, par exemple avec les
+        informations issues du Device Tree */
+        acceldev->used_channel = 0;
+
+
+        /* Initialise la partie miscdevice de foo_device */
+        acceldev->miscdev.minor = MISC_DYNAMIC_MINOR;
+        acceldev->miscdev.name = "accelX";
+        acceldev->miscdev.fops = &accel_fops;
+        acceldev->miscdev.parent = &client->dev; /* (1) */
+
+        i2c_set_clientdata(client,acceldev );
+
+        /* S'enregistre auprès du framework misc */
+        ret = misc_register(&acceldev->miscdev);
+
+        // sending init i2c paquets
+        init_accel(client);
         return 0;
 }
 
